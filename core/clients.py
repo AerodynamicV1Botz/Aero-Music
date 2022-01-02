258b04b1fc47ece903290e987043f78833352b2d
Anonymous Boy <rahulking25102006@gmail.com>
2022-01-03 02:14:28 +0530

    Update clients.py

--- core/clients.py
+++ core/clients.py
@@ -1,17 +1,21 @@
-from pyrogram import Client
 from pytgcalls import PyTgCalls
+
 from configs import config
+from pyrogram import Client
 
 
-user = Client(config.SESSION, config.API_ID, config.API_HASH)
+user = Client(
+    config.SESSION,
+    config.API_ID,
+    config.API_HASH
+)
 
-
-bot = Client(
+pyro_bot = Client(
     ":memory:",
     config.API_ID,
     config.API_HASH,
     bot_token=config.BOT_TOKEN,
-    plugins={"root": "plugins"},
+    plugins={"root": "plugins"}
 )
 
 call_py = PyTgCalls(user)
