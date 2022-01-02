ffed322c488b9340dfc3e69730662ecdbe8379ed
Anonymous Boy <rahulking25102006@gmail.com>
2022-01-03 03:01:06 +0530

    Update callback_handlers.py

--- plugins/callback_handlers.py
+++ plugins/callback_handlers.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timedelta
+
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
 
@@ -11,9 +13,9 @@ from functions.youtube_utils import (
 )
 from functions.markup_button import process_button, start_markup
 
-from database.lang_utils import get_message as gm
-from database.chat_database import ChatDB
-from . import helps, paginate_module, modules, load_module
+from database.lang_utils import gm
+from database.chat_database import chat_db
+from . import helps, paginate_module, modules, load_modules
 
 
 @Client.on_callback_query(filters.regex(pattern=r"(back|next)(music|video)\|(\d+)"))
@@ -24,7 +26,7 @@ async def _button_cb(_, cb: CallbackQuery):
     user_id = int(match(3))
     chat_id = cb.message.chat.id
     if cb.from_user.id != user_id:
-        return await cb.answer(gm(chat_id, "not_for_you"), show_alert=True)
+        return await cb.answer(await gm(chat_id, "not_for_you"), show_alert=True)
     yt_btn = process_button(user_id, music_or_video)
     if next_or_back == "next":
         next_search(chat_id)
@@ -39,10 +41,22 @@ async def _button_cb(_, cb: CallbackQuery):
             InlineKeyboardButton("🗑️", f"close|{user_id}"),
             InlineKeyboardButton("➡️", f"next{music_or_video}|{user_id}"),
         ]
-    text = extract_info(chat_id, stream_result)
+    text = await extract_info(chat_id, stream_result)
     await cb.edit_message_text(
         text, reply_markup=InlineKeyboardMarkup([yt_btn[0], yt_btn[1], btn])
     )
+
+
+async def check_duration(chat_id, date_time, cb):
+    duration = (date_time - datetime(1900, 1, 1)).total_seconds()
+    duration_limit = int((await chat_db.get_chat(chat_id))["duration"])
+    duration_limit = duration_limit * 60
+    if duration >= duration_limit:
+        return await cb.answer(
+            await gm(chat_id, "duration_reach_limit", [str(timedelta(seconds=duration_limit))]),
+            show_alert=True
+        )
+    pass
 
 
 @Client.on_callback_query(filters.regex(pattern=r"((video|music) ((\d)\|(\d+)))"))
@@ -61,6 +75,15 @@ async def _music_or_video(_, cb: CallbackQuery):
         "yt_id": result["yt_id"],
         "stream_type": stream_type,
     }
+    try:
+        date_time = datetime.strptime(res["duration"], "%H:%M:%S")
+        await check_duration(chat_id, date_time, cb)
+    except ValueError:
+        try:
+            date_time = datetime.strptime(res["duration"], "%M:%S")
+            await check_duration(chat_id, date_time, cb)
+        except ValueError:
+            pass
     await player.music_or_video(cb, res)
 
 
@@ -79,7 +102,7 @@ async def _close_button(_, cb: CallbackQuery):
     if member.status in ["creator", "administrator"]:
         return await cb.message.delete()
     if cb.from_user.id != user_id or not user_id:
-        return await cb.answer(gm(cb.message.chat.id, "not_for_you"), show_alert=True)
+        return await cb.answer(await gm(cb.message.chat.id, "not_for_you"), show_alert=True)
     if modules:
         modules.clear()
 
@@ -88,11 +111,11 @@ async def _close_button(_, cb: CallbackQuery):
 async def _change_lang(_, cb: CallbackQuery):
     lang = cb.matches[0].group(1)
     chat_id = cb.message.chat.id
-    set_lang = ChatDB().set_lang(chat_id, lang)
+    set_lang = await chat_db.set_lang(chat_id, lang)
     await cb.message.edit(
-        gm(chat_id, set_lang),
+        await gm(chat_id, set_lang),
         reply_markup=InlineKeyboardMarkup(
-            [[InlineKeyboardButton(gm(chat_id, "channel"), url=config.CHANNEL_LINK)]]
+            [[InlineKeyboardButton(await gm(chat_id, "channel"), url=config.CHANNEL_LINK)]]
         ),
     )
 
@@ -102,8 +125,8 @@ async def goback(client: Client, hee: CallbackQuery):
     bot_username = (await client.get_me()).username
     chid = hee.message.chat.id
     await hee.edit_message_text(
-        gm(chid, "pm_greet").format(hee.message.from_user.mention),
-        reply_markup=start_markup(chid, bot_username),
+        await gm(chid, "pm_greet", [str(hee.message.from_user.mention)]),
+        reply_markup=await start_markup(chid, bot_username),
     )
 
 
@@ -111,32 +134,33 @@ async def goback(client: Client, hee: CallbackQuery):
 async def cbhelp(_, lol: CallbackQuery):
     match = lol.matches[0].group(1)
     chat_id = lol.message.chat.id
-    user_id = int(lol.matches[0].group(3))
     if match == "cbhelp":
+        user_id = lol.from_user.id
         return await lol.edit_message_text(
-            gm(chat_id, "helpmusic"),
+            await gm(chat_id, "helpmusic"),
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
                         InlineKeyboardButton(
-                            gm(chat_id, "commands"), callback_data=f"plug_back|{user_id}"
+                            await gm(chat_id, "commands"), callback_data=f"plug_back|{user_id}"
                         ),
                         InlineKeyboardButton(
-                            gm(chat_id, "backtomenu"), callback_data="goback"
+                            await gm(chat_id, "backtomenu"), callback_data="goback"
                         )
                     ]
                 ]
             ),
         )
+    user_id = int(lol.matches[0].group(3))
     if match == f"plug_back|{user_id}":
         from_user_id = lol.from_user.id
         if from_user_id != user_id:
-            return await lol.answer(gm(chat_id, "not_for_you"), show_alert=True)
+            return await lol.answer(await gm(chat_id, "not_for_you"), show_alert=True)
         if not modules:
-            load_module(user_id)
-        keyboard = paginate_module(chat_id, user_id)
+            await load_modules(user_id)
+        keyboard = await paginate_module(chat_id, user_id)
         await lol.edit_message_text(
-            gm(chat_id, "here_all_commands"),
+            await gm(chat_id, "here_all_commands"),
             reply_markup=InlineKeyboardMarkup(keyboard)
         )
 
@@ -148,14 +172,14 @@ async def cb_help_plugins_(_, cb: CallbackQuery):
     from_user_id = cb.from_user.id
     chat_id = cb.message.chat.id
     if from_user_id != user_id:
-        return await cb.answer(gm(chat_id, "not_for_you"), show_alert=True)
+        return await cb.answer(await gm(chat_id, "not_for_you"), show_alert=True)
     items = helps[module]
     module_name = f"{module.split('plugins.')[1].title()}"
     result = ""
     for key, value in items.items():
-        result += f"/{key}:    {gm(chat_id, value)}\n"
+        result += f"/{key}:    {await gm(chat_id, value)}\n"
     return await cb.edit_message_text(
-        gm(chat_id, "help_for").format(module_name, result),
+        await gm(chat_id, "help_for", [module_name, result]),
         reply_markup=InlineKeyboardMarkup(
             [
                 [
