851014db9283f77221b24fd14f34271f71963684
Anonymous Boy <rahulking25102006@gmail.com>
2022-01-03 02:16:28 +0530

    Update queue.py

--- core/queue.py
+++ core/queue.py
@@ -1,31 +1,31 @@
 from typing import Dict, List
 
 
-class Queue:
+class Playlist:
     def __init__(self):
         self.__queue: Dict[int, List[Dict[str, str]]] = {}
 
-    def insert_one(self, chat_id: int, objects: Dict[str, str]):
+    def insert_one(self, chat_id: int, data: Dict[str, str]):
         if chat_id not in self.__queue:
-            self.__queue[chat_id] = [objects]
-            queue = self.__queue[chat_id]
+            self.__queue[chat_id] = [data]
         else:
             queue = self.__queue[chat_id]
-            queue.extend([objects])
+            queue.extend([data])
 
     def delete_one(self, chat_id: int):
-        if chat_id not in self.__queue:
-            return "not_in_queue"
-        self.__queue[chat_id].pop(0)
+        return "not_in_queue" if chat_id not in self.__queue else self.__queue[chat_id].pop(0)
 
     def delete_chat(self, chat_id: int):
         if chat_id not in self.__queue:
             return "not_in_queue"
         del self.__queue[chat_id]
 
-    def get(self, chat_id: int):
+    def get_queue(self, chat_id: int):
         return self.__queue[chat_id][0]
 
     @property
-    def playlist(self):
+    def playlists(self):
         return self.__queue
+
+
+playlist = Playlist()
