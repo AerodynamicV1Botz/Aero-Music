9ba83ce7a9153086ca614110455e8ba13e37f64a
Anonymous Boy <rahulking25102006@gmail.com>
2022-01-03 03:07:57 +0530

    Update stream.py

--- plugins/stream.py
+++ plugins/stream.py
@@ -2,7 +2,7 @@ from pyrogram import Client, filters, types
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 from core.player import player
-from database.lang_utils import get_message
+from database.lang_utils import gm
 from functions.youtube_utils import (
     extract_info,
     stream_result,
@@ -12,10 +12,10 @@ from functions.decorators import only_admin
 from functions.markup_button import process_button
 
 
-def extract_all(query: str, chat_id: int, user_id: int, status: str):
+async def extract_all(query: str, chat_id: int, user_id: int, status: str):
     yt_btn = process_button(user_id, status)
     yt_search(chat_id, query)
-    result = extract_info(chat_id, stream_result)
+    result = await extract_info(chat_id, stream_result)
     return result, yt_btn
 
 
@@ -29,7 +29,7 @@ async def play_(_, message: types.Message):
         return await player.local_music(user_id, reply)
     command = message.command
     if len(command) == 1:
-        return await message.reply(get_message(chat_id, "wrong_play_vplay_input"))
+        return await message.reply(await gm(chat_id, "wrong_play_vplay_input"))
     query = " ".join(command[1:])
     status = "music"
     result, yt_btn = extract_all(query, chat_id, user_id, status)
@@ -40,8 +40,8 @@ async def play_(_, message: types.Message):
                 yt_btn[0],
                 yt_btn[1],
                 [
-                    InlineKeyboardButton("ðŸ—‘", f"close|{user_id}"),
-                    InlineKeyboardButton("âž¡", f"nextmusic|{user_id}"),
+                    InlineKeyboardButton("—", f"close|{user_id}"),
+                    InlineKeyboardButton("—", f"nextmusic|{user_id}"),
                 ],
             ],
         ),
@@ -58,7 +58,7 @@ async def vplay_(_, message: types.Message):
         return await player.local_video(user_id, reply)
     commands = message.command
     if len(commands) == 1:
-        return await message.reply(get_message(chat_id, "wrong_play_vplay_input"))
+        return await message.reply(await gm(chat_id, "wrong_play_vplay_input"))
     query = " ".join(commands[1:])
     status = "video"
     result, yt_btn = extract_all(query, chat_id, user_id, status)
@@ -69,8 +69,8 @@ async def vplay_(_, message: types.Message):
                 yt_btn[0],
                 yt_btn[1],
                 [
-                    InlineKeyboardButton("ðŸ—‘", f"close|{user_id}"),
-                    InlineKeyboardButton("âž¡", f"nextvideo|{user_id}"),
+                    InlineKeyboardButton("—", f"close|{user_id}"),
+                    InlineKeyboardButton("—", f"nextvideo|{user_id}"),
                 ],
             ]
         ),
