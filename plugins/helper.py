71295ebb447cca47e733ac49ca4429f4f92fc794
Anonymous Boy <rahulking25102006@gmail.com>
2022-01-03 03:02:32 +0530

    Update helper.py

--- plugins/helper.py
+++ plugins/helper.py
@@ -1,18 +1,22 @@
-from . import paginate_module, load_module, modules
+from . import paginate_module, load_modules, modules
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 
-from database.lang_utils import get_message as gm, get_message
+from database.lang_utils import gm
 
 
 @Client.on_message(filters.command("help"))
 async def help_cmds_(_, message: Message):
     chat_id = message.chat.id
     user_id = message.from_user.id
-    load_module(user_id)
-    keyboard = paginate_module(chat_id, user_id)
+    if modules:
+        modules.clear()
+    await load_modules(user_id)
+    keyboard = await paginate_module(chat_id, user_id)
+    keyboard.pop(-1)
+    keyboard[-1].append(InlineKeyboardButton(f"🗑️ {await gm(chat_id, 'close_btn_name')}", f"close|{user_id}"))
     await message.reply(
-        get_message(chat_id, "here_all_commands"),
+        await gm(chat_id, "here_all_commands"),
         reply_markup=InlineKeyboardMarkup(keyboard)
     )
     modules.clear()
