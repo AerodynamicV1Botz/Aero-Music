d79159a5db21872bc716f9e28654044d9083e1b0
Anonymous Boy <rahulking25102006@gmail.com>
2022-01-03 03:00:24 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -1,51 +1,58 @@
 import asyncio
 
-from pyrogram import filters, Client
+from pyrogram import Client, filters
 from pyrogram.types import Message
-
 from configs import config
-from core.bot import Bot
+from core.bot import bot
 from core.clients import user
-from database.lang_utils import get_message as gm
-from database.chat_database import ChatDB
+from database.lang_utils import gm
+from database.db import dbs
 
 
-@Client.on_message(filters.command("gcast") & filters.user(config.OWNER_ID))
-async def gcast_(client: Client, message: Message):
-    if message.reply_to_message:
-        text = message.reply_to_message.text
+@Client.on_message(filters.command("gcast"))
+async def gcast_(client: Client, m: Message):
+    if m.reply_to_message:
+        text = m.reply_to_message.text
     else:
-        text = message.text[7:]
-    msg = await message.reply(gm(message.chat.id, "process_gcast"))
+        text = m.text[7:]
+    chat_id = m.chat.id
+    from_user_id = m.from_user.id
+    if from_user_id != config.OWNER_ID:
+        return await bot.send_message(chat_id, "you_not_owner")
+    msg = await m.reply(await gm(chat_id, "process_gcast"))
     error = success = 0
-    gcast_type = ChatDB().get_chat(message.chat.id)[0]["gcast_type"]
+    gcast_type = (await dbs.get_chat(chat_id))["gcast_type"]
     sender = user if gcast_type == "user" else client
-    async for dialog in user.iter_dialogs():
-        if dialog.chat.type in ["group", "supergroup"]:
-            chat_id = dialog.chat.id
+    chat_lists = await dbs.get_chat_id()
+    for chat in chat_lists:
+        chat_ids = str(chat)
+        if chat_ids.startswith("-"):
             try:
                 success += 1
-                await asyncio.sleep(3)
-                await sender.send_message(chat_id, text)
-            except Exception as e:
+                await asyncio.sleep(2.5)
+                await sender.send_message(int(chat_ids), text)
+            except Exception as e:  # noqa
                 print(e)
                 error += 1
     return await msg.edit(
-        gm(message.chat.id, "success_gcast").format(str(success), str(error))
+        await gm(chat_id, "success_gcast", [str(success), str(error)])
     )
 
 
-@Client.on_message(filters.command("setgcast") & filters.user(config.OWNER_ID))
+@Client.on_message(filters.command("setgcast"))
 async def set_gcast_(_, message: Message):
     chat_id = message.chat.id
+    from_user_id = message.from_user.id
+    if from_user_id != config.OWNER_ID:
+        return await bot.send_message(chat_id, "you_not_owner")
     try:
         gcast_type = message.command[1]
     except IndexError:
         return await message.reply("Give me an input")
     if gcast_type not in ["bot", "user"]:
-        return await message.reply(gm(chat_id, "invalid_gcast_type"))
-    key = ChatDB().set_gcast(chat_id, gcast_type)
-    return await Bot().send_message(chat_id, key, gcast_type)
+        return await message.reply(await gm(chat_id, "invalid_gcast_type"))
+    key = await dbs.set_gcast_type(chat_id, gcast_type)
+    return await bot.send_message(chat_id, key, [gcast_type])
 
 
 __cmds__ = ["gcast", "setgcast"]
