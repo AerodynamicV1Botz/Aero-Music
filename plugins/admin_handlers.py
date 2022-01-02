386cf2f049df2acaac9a3f7e78e8d3dfe2c14c04
Anonymous Boy <rahulking25102006@gmail.com>
2022-01-03 02:58:55 +0530

    Update admin_handlers.py

--- plugins/admin_handlers.py
+++ plugins/admin_handlers.py
@@ -1,66 +1,54 @@
-import sys
+from sys import executable
+from os import execle, environ
 
-from pyrogram import Client, filters, types
+from pyrogram import Client, filters
+from pyrogram.types import Message
 
 from configs import config
-from core.bot import Bot
 from core.player import player
+
+from database.lang_utils import gm
 from functions.decorators import authorized_only
-from os import execle, environ
-from database.lang_utils import get_message as gm
 
 
-@Client.on_message(filters.command("pause"))
+@Client.on_message(filters.command(["pause", "resume"]))
 @authorized_only
-async def pause(_, message: types.Message):
-    chat_id = message.chat.id
-    stats = await player.change_streaming_status("pause", chat_id)
-    return await Bot().send_message(
-        chat_id,
-        stats,
-    )
-
-
-@Client.on_message(filters.command("resume"))
-@authorized_only
-async def resume_(_, message: types.Message):
-    chat_id = message.chat.id
-    stats = await player.change_streaming_status("resume", chat_id)
-    return await Bot().send_message(chat_id, stats)
+async def pause(_, m: Message):
+    chat_id = m.chat.id
+    status = m.command[1]
+    return await player.change_streaming_status(chat_id, status)
 
 
 @Client.on_message(filters.command("skip"))
 @authorized_only
-async def skip_(_, message: types.Message):
-    chat_id = message.chat.id
-    await player.change_stream(chat_id)
+async def skip_(_, m: Message):
+    chat_id = m.chat.id
+    return await player.change_stream(chat_id)
 
 
 @Client.on_message(filters.command(["vol", "volume"]))
 @authorized_only
-async def change_vol_(_, message: types.Message):
-    chat_id = message.chat.id
-    vol = int(message.command[1])
-    await player.change_vol(chat_id, vol)
+async def change_vol_(_, m: Message):
+    chat_id = m.chat.id
+    volume = int(m.command[1])
+    return await player.change_vol(chat_id, volume)
 
 
 @Client.on_message(filters.command("end"))
 @authorized_only
-async def end_stream_(_, message: types.Message):
-    chat_id = message.chat.id
-    key = await player.end_stream(chat_id)
-    first_name = (await message.chat.get_member(message.from_user.id)).user.first_name
-    return await Bot().send_message(chat_id, key, first_name)
+async def end_stream_(_, m: Message):
+    chat_id = m.chat.id
+    first_name = m.from_user.first_name
+    return await player.end_stream(chat_id, first_name)
 
 
 @Client.on_message(filters.command("restart") & filters.user(config.OWNER_ID))
-async def restart_bot_(client: Client, message: types.Message):
-    chat_id = message.chat.id
-    msg = await message.reply(gm(chat_id, "restart_bot"))
-    args = [sys.executable, "main.py"]
-    await msg.edit(gm(chat_id, "restarted"))
-    execle(sys.executable, *args, environ)
-    await client.send_message(chat_id, "Hey, Jinda hu bsdk")
+async def restart_bot_(_, m: Message):
+    chat_id = m.chat.id
+    msg = await m.reply(await gm(chat_id, "restart_bot"))
+    args = [executable, "main.py"]
+    await msg.edit(await gm(chat_id, "restarted"))
+    execle(executable, *args, environ)  # noqa
     return
 
 
