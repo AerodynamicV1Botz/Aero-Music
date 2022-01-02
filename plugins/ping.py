f8ccf12c631f7076912d487d0f34424e57c2da70
Anonymous Boy <rahulking25102006@gmail.com>
2022-01-03 03:04:52 +0530

    Update ping.py

--- plugins/ping.py
+++ plugins/ping.py
@@ -4,19 +4,21 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 
 from core.clients import call_py
-from database.lang_utils import get_message as gm
+from database.lang_utils import gm
 
 
 @Client.on_message(filters.command("ping"))
 async def check_ping_(_, message: Message):
     chat_id = message.chat.id
     start = time()
-    msg = await message.reply(gm(chat_id, "ping_text"))
+    msg = await message.reply(await gm(chat_id, "ping_text"))
     pytgcalls_latency = await call_py.ping
     pyrogram_latency = time() - start
     await msg.edit(
-        gm(chat_id, "pong_text").format(
-            round(pyrogram_latency * 1000, 3), round(pytgcalls_latency, 2)
+        await gm(
+            chat_id,
+            "pong_text",
+            [str(round(pyrogram_latency * 1000, 3)), str(round(pytgcalls_latency, 2))]
         )
     )
 
