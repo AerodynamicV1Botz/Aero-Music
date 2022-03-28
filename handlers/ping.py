77e82582a39844b3b14573a2034d577b46c3de7e
Anonymous <rahulking25102006@gmail.com>
2022-03-28 10:10:06 +0530

    Update ping.py

--- handlers/ping.py
+++ handlers/ping.py
@@ -1,4 +1,5 @@
 import asyncio
+from time import time
 from helpers.filters import command
 from config import BOT_USERNAME, SUPPORT_GROUP
 from pyrogram import Client, filters
@@ -8,10 +9,12 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 @Client.on_message(command(["ping", "repo", "anon", "alive"]) & filters.group & ~filters.edited & ~filters.private)
 
 async def help(client: Client, message: Message):
+    start = time()
+    delta_ping = time() - start
     await message.reply_sticker("CAACAgUAAxkBAAEENxZiNtPdibVkMsjLZrUG9NK4hotHQgAC2wEAAoM12VSdN9ujxVtnUyME")
     await message.reply_photo(
         photo="https://telegra.ph/file/89cbc8b8760b6abff430f.jpg",
-        caption=f"""<b>🏓 ᴩᴏɴɢ ʙᴀʙʏ !</b>""",
+        caption=f"""<b>🏓 ᴩᴏɴɢ ʙᴀʙʏ !</b>\n`{delta_ping * 1000:.3f} ᴍs`""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
