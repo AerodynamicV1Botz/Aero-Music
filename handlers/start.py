a1e8002f3bd86b192ef2c3deffd0cd74fde5b068
ᴀɴᴏɴʏᴍᴏᴜs <rahulking25102006@gmail.com>
2022-04-17 19:30:21 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -8,6 +8,7 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
 @Client.on_message(command("start") & filters.private & ~filters.group & ~filters.edited)
 async def start_(client: Client, message: Message):
+    await message.delete()
     await message.reply_sticker("CAACAgUAAxkBAAEENxZiNtPdibVkMsjLZrUG9NK4hotHQgAC2wEAAoM12VSdN9ujxVtnUyME")
     await message.reply_photo(
         photo=f"{START_IMG}",
