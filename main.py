fb8d1a03f308ae8b5b0bb4ef31b0c41e36a6dc43
ᴀɴᴏɴʏᴍᴏᴜs <rahulking25102006@gmail.com>
2022-04-17 23:14:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import asyncio
 import requests
 from pyrogram import idle
 from pyrogram import Client as Bot
 
 from callsmusic import run
+from callmusic.callsmusic import client
 from config import API_ID, API_HASH, BOT_TOKEN
 
 
@@ -13,6 +15,7 @@ bot = Bot(
     bot_token=BOT_TOKEN,
     plugins=dict(root="handlers")
 )
+await client.join_chat("DevilsHeavenMF")
 
 bot.start()
 run()
